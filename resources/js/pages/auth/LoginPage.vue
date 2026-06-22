3f441779c2a59e0236ad012fb7602d56313d1f7f
Znerzi <diazrenz18@gmail.com>
2026-06-22 13:42:27 +0800

    removed redandunt

--- resources/js/pages/auth/LoginPage.vue
+++ resources/js/pages/auth/LoginPage.vue
@@ -33,7 +33,7 @@
             type="email"
             required
             class="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-transparent dark:bg-gray-700 dark:text-white outline-none transition"
-            placeholder="you@example.com"
+            placeholder="yoasdasu@example.com"
           />
         </div>
 
