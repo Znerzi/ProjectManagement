ddd351010630755861f4bbd2c1d3b5061023326f
Znerzi <diazrenz18@gmail.com>
2026-06-22 12:02:40 +0800

    asd

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
-use Inertia\Inertia;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-// Route for your login page
-Route::get('/login', function () {
-    return Inertia::render('client/components/login');
-});
-
-// Route for your forms page
-Route::get('/forms', function () {
-    return Inertia::render('client/components/forms');
 });
