24f8f5afb877a56cce1f9f358aee5827f18028b0
Znerzi <diazrenz18@gmail.com>
2026-06-22 14:10:42 +0800

    login

--- resources/js/pages/auth/LoginPage.vue
+++ resources/js/pages/auth/LoginPage.vue
@@ -8,7 +8,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
           </svg>
         </div>
-        <h1 class="text-2xl font-bold text-gray-900 dark:text-white">Welcome Back</h1>
+        <h1 class="text-2xl font-bold text-gray-900 dark:text-white">Welcomasdasde Back</h1>
         <p class="text-gray-600 dark:text-gray-400 mt-2">Sign in to your account</p>
       </div>
 
